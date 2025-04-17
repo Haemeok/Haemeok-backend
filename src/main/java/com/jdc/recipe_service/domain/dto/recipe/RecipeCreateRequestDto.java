b4a8b6d876617c5200f6ab6c7b96b9a84fe833bf
minseo0943 <minseo0943@naver.com>
2025-04-17 19:26:51 +0900

    평점에서 댓글 연동 & 레시피 생성 오류설정 추가

--- src/main/java/com/jdc/recipe_service/domain/dto/recipe/RecipeCreateRequestDto.java
+++ src/main/java/com/jdc/recipe_service/domain/dto/recipe/RecipeCreateRequestDto.java
@@ -2,6 +2,7 @@
 
     import com.jdc.recipe_service.domain.dto.recipe.ingredient.RecipeIngredientRequestDto;
     import com.jdc.recipe_service.domain.dto.recipe.step.RecipeStepRequestDto;
+    import jakarta.validation.constraints.NotBlank;
     import lombok.AllArgsConstructor;
     import lombok.Builder;
     import lombok.Getter;
@@ -19,9 +20,15 @@
     @AllArgsConstructor
     @NoArgsConstructor
     public class RecipeCreateRequestDto {
+
+        @NotBlank(message = "레시피 제목은 필수입니다.")
         private String title;
+
         private String description;
+
+        @NotBlank(message = "요리 유형은 필수입니다.")
         private String dishType;
+
         private Integer cookingTime;
         private String imageKey; // 🔄 imageUrl → imageKey (S3 key 저장용)
         private List<String> stepImageKeys;
