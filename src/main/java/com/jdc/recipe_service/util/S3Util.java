24c661546f19da6797d232d85cfee543f4addc6b
minseo0943 <minseo0943@naver.com>
2025-06-07 21:43:38 +0900

    actionImage 로직 변경

--- src/main/java/com/jdc/recipe_service/util/S3Util.java
+++ src/main/java/com/jdc/recipe_service/util/S3Util.java
@@ -28,9 +28,29 @@ public class S3Util {
     private String bucketName;
 
     /**
+     * 버킷이 존재하지 않으면 생성
+     */
+    public void ensureBucketExists() {
+        try {
+            HeadBucketRequest headBucket = HeadBucketRequest.builder()
+                    .bucket(bucketName)
+                    .build();
+            s3Client.headBucket(headBucket);
+        } catch (NoSuchBucketException e) {
+            log.info("버킷이 존재하지 않아 새로 생성합니다: {}", bucketName);
+            CreateBucketRequest createBucket = CreateBucketRequest.builder()
+                    .bucket(bucketName)
+                    .build();
+            s3Client.createBucket(createBucket);
+        }
+    }
+
+    /**
      * presigned PUT URL 생성 (유저 업로드용)
      */
     public String createPresignedUrl(String fileKey) {
+        ensureBucketExists();
+
         log.info("📦 S3Presigner class: {}", s3Presigner.getClass().getName());
 
         PutObjectRequest objectRequest = PutObjectRequest.builder()
@@ -92,5 +112,36 @@ public class S3Util {
         } catch (S3Exception e) {
             return false;
         }
+    }
+
+    /**
+     * 외부 URL(externalUrl)에 있는 이미지를 스트리밍 받아,
+     * S3에 "s3Key" 위치로 업로드한 뒤, S3 key를 반환한다.
+     *
+     * @param externalUrl 외부(예: DALL·E)에서 생성된 이미지 URL
+     * @param s3Key       S3에 저장할 key (예: "recipes/42/123/ai-generated-main.jpg")
+     * @return 실제 업로드된 S3 key
+     * @throws Exception 다운로드 혹은 S3 업로드 중 에러 발생 시
+     */
+    public String uploadFromUrl(String externalUrl, String s3Key) throws Exception {
+        ensureBucketExists();
+
+        URL url = new URL(externalUrl);
+        URLConnection conn = url.openConnection();
+        String contentType = conn.getContentType();
+        long contentLength = conn.getContentLengthLong();
+        InputStream inputStream = conn.getInputStream();
+
+        PutObjectRequest putReq = PutObjectRequest.builder()
+                .bucket(bucketName)
+                .key(s3Key)
+                .contentType(contentType)
+                .contentLength(contentLength)
+                .build();
+
+        s3Client.putObject(putReq, RequestBody.fromInputStream(inputStream, contentLength));
+        inputStream.close();
+
+        return s3Key;
     }
 }
