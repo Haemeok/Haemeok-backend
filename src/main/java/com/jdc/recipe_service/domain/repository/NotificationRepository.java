f509a79e02fe1d6990db1ea900ea4f3bd8d54b68
coldcity2 <cgh9658@gmail.com>
2025-07-02 14:46:08 +0900

    feat(notification): JPA 레포지토리 추가

---
+++ src/main/java/com/jdc/recipe_service/domain/repository/NotificationRepository.java
@@ -0,0 +1,10 @@
+package com.jdc.recipe_service.domain.repository;
+
+import com.jdc.recipe_service.domain.entity.Notification;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+public interface NotificationRepository extends JpaRepository<Notification, Long> {
+    List<Notification> findByUserIdOrderByCreatedAtDesc(Long userId);
+    List<Notification> findByUserIdAndIsReadFalse(Long userId);
+}
