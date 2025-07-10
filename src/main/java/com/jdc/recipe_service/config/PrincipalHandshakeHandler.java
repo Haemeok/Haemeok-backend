abe9b748573757432936e5c95be26b4fbf44efcf
coldcity2 <cgh9658@gmail.com>
2025-07-10 21:55:32 +0900

    feat(websocket): 핸드쉐이크 단계에서 JWT 인증 적용 및 STOMP 메시지 보안 강화

--- src/main/java/com/jdc/recipe_service/config/PrincipalHandshakeHandler.java
+++ src/main/java/com/jdc/recipe_service/config/PrincipalHandshakeHandler.java
@@ -2,6 +2,7 @@ package com.jdc.recipe_service.config;
 
 import org.springframework.http.server.ServerHttpRequest;
 import org.springframework.lang.NonNull;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.server.support.DefaultHandshakeHandler;
 
@@ -15,7 +16,10 @@ public class PrincipalHandshakeHandler extends DefaultHandshakeHandler {
             @NonNull WebSocketHandler wsHandler,
             @NonNull Map<String, Object> attributes
     ) {
-        Object principal = attributes.get("user");
-        return (principal instanceof Principal ? (Principal) principal : super.determineUser(request, wsHandler, attributes));
+        Object user = attributes.get("user");
+        if (user instanceof Authentication) {
+            return (Authentication) user;
+        }
+        return super.determineUser(request, wsHandler, attributes);
     }
 }
--- src/main/java/com/jdc/recipe_service/config/WebSocketConfig.java
+++ src/main/java/com/jdc/recipe_service/config/WebSocketConfig.java
@@ -4,17 +4,7 @@ import com.jdc.recipe_service.jwt.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.messaging.Message;
-import org.springframework.messaging.MessageChannel;
-import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.messaging.simp.stomp.StompCommand;
-import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
-import org.springframework.messaging.support.ChannelInterceptor;
-import org.springframework.messaging.support.MessageHeaderAccessor;
-import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.core.Authentication;
-import org.springframework.util.StringUtils;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
@@ -36,40 +26,8 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws/notifications")
                 .setAllowedOriginPatterns("http://localhost:5173", "https://www.haemeok.com")
+                .addInterceptors(new JwtHandshakeInterceptor(jwtTokenProvider))
+                .setHandshakeHandler(new PrincipalHandshakeHandler())
                 .withSockJS();
-    }
-
-    @Override
-    public void configureClientInboundChannel(ChannelRegistration registration) {
-        registration.interceptors(new ChannelInterceptor() {
-            @Override
-            public Message<?> preSend(Message<?> message, MessageChannel channel) {
-                StompHeaderAccessor accessor =
-                        MessageHeaderAccessor.getAccessor(message, StompHeaderAccessor.class);
-
-                if (StompCommand.CONNECT.equals(accessor.getCommand())) {
-                    log.debug("STOMP CONNECT attempt");
-
-                    String bearerToken = accessor.getFirstNativeHeader("Authorization");
-                    if (StringUtils.hasText(bearerToken) && bearerToken.startsWith("Bearer ")) {
-                        String token = bearerToken.substring(7);
-                        log.debug("Token found, attempting to authenticate.");
-
-                        if (jwtTokenProvider.validateToken(token)) {
-                            Authentication auth = jwtTokenProvider.getAuthentication(token);
-                            accessor.setUser(auth);
-                            log.info("STOMP user authenticated: {}", auth.getName());
-                        } else {
-                            log.warn("Invalid JWT token received.");
-                            throw new BadCredentialsException("Invalid token");
-                        }
-                    } else {
-                        log.warn("STOMP CONNECT without token.");
-                        throw new BadCredentialsException("Missing token");
-                    }
-                }
-                return message;
-            }
-        });
     }
 }
