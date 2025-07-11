c48e1401539090a1c6e24669dbc3ccf3c5d69c05
coldcity2 <cgh9658@gmail.com>
2025-07-11 15:11:08 +0900

    feat(notification): 전체 삭제 및 개별 읽음 처리 기능 추가

--- src/main/java/com/jdc/recipe_service/domain/repository/NotificationRepository.java
+++ src/main/java/com/jdc/recipe_service/domain/repository/NotificationRepository.java
@@ -9,4 +9,5 @@ public interface NotificationRepository extends JpaRepository<Notification, Long
     List<Notification> findByUserIdAndIsReadFalse(Long userId);
     List<Notification> findByUserIdAndIsReadFalseOrderByCreatedAtDesc(Long userId);
     List<Notification> findByUserIdAndIsReadTrueOrderByCreatedAtDesc(Long userId);
+    void deleteByUserId(Long userId);
 }
