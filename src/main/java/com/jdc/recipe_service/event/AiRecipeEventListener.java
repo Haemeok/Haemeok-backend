84271c4efe48dd8df58f46dbf602cc98b25f3ba0
minseo0943 <minseo0943@naver.com>
2025-10-23 15:08:59 +0900

    feat: 이미지 API imagen-3.0으로 변경

--- src/main/java/com/jdc/recipe_service/event/AiRecipeEventListener.java
+++ src/main/java/com/jdc/recipe_service/event/AiRecipeEventListener.java
@@ -3,7 +3,9 @@ package com.jdc.recipe_service.event;
 import com.jdc.recipe_service.domain.dto.notification.NotificationCreateDto;
 import com.jdc.recipe_service.domain.type.NotificationRelatedType;
 import com.jdc.recipe_service.domain.type.NotificationType;
+import com.jdc.recipe_service.opensearch.service.RecipeIndexingService;
 import com.jdc.recipe_service.service.AsyncImageService;
+import com.jdc.recipe_service.service.AsyncImageServiceV2;
 import com.jdc.recipe_service.service.NotificationService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -16,12 +18,20 @@ import org.springframework.transaction.event.TransactionalEventListener;
 @RequiredArgsConstructor
 @Slf4j
 public class AiRecipeEventListener {
-    private final AsyncImageService asyncImageService;
+    private final AsyncImageServiceV2 asyncImageService;
     private final NotificationService notificationService;
+    private final RecipeIndexingService recipeIndexingService;
 
     @Async
     @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
     public void onAiRecipeCreated(AiRecipeCreatedEvent event) {
+
+        try {
+            recipeIndexingService.indexRecipeSafelyWithRetry(event.getRecipeId());
+        } catch (Exception e) {
+            log.error("AI 레시피 생성 후 OpenSearch 인덱싱 시도 실패 (리스너 레벨)", e);
+        }
+
         asyncImageService.generateAndUploadAiImageAsync(event.getRecipeId())
                 .thenAccept(imageUrl -> {
                     notificationService.createNotification(
