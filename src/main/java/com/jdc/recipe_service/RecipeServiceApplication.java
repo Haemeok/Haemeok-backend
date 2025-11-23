19e158db4de76e09804e0bfc5adde7f4bbf2c8ce
coldcity2 <cgh9658@gmail.com>
2025-11-23 15:45:01 +0900

    feat(app): KST TimeZone을 기본값으로 설정

--- src/main/java/com/jdc/recipe_service/RecipeServiceApplication.java
+++ src/main/java/com/jdc/recipe_service/RecipeServiceApplication.java
@@ -1,11 +1,14 @@
 package com.jdc.recipe_service;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import java.util.TimeZone;
 
 @SpringBootApplication
 @EnableJpaAuditing
@@ -16,6 +19,11 @@ public class RecipeServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RecipeServiceApplication.class, args);
+	}
+
+	@PostConstruct
+	public void init() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
 	}
 
 }
