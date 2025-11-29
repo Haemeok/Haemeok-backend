dc8b03c65aa25194c0929f44f1fd2612d3030729
coldcity2 <cgh9658@gmail.com>
2025-11-30 03:28:41 +0900

    refactor(domain): 엔티티 영양성분 필드 정밀도 상향

--- src/main/java/com/jdc/recipe_service/domain/entity/CookingRecord.java
+++ src/main/java/com/jdc/recipe_service/domain/entity/CookingRecord.java
@@ -38,22 +38,22 @@ public class CookingRecord {
     @Column(nullable = false)
     private Integer savings;
 
-    @Column(name = "protein", precision = 10, scale = 2)
+    @Column(name = "protein", precision = 10, scale = 3)
     private BigDecimal protein;
 
-    @Column(name = "carbohydrate", precision = 10, scale = 2)
+    @Column(name = "carbohydrate", precision = 10, scale = 3)
     private BigDecimal carbohydrate;
 
-    @Column(name = "fat", precision = 10, scale = 2)
+    @Column(name = "fat", precision = 10, scale = 3)
     private BigDecimal fat;
 
-    @Column(name = "sugar", precision = 10, scale = 2)
+    @Column(name = "sugar", precision = 10, scale = 3)
     private BigDecimal sugar;
 
-    @Column(name = "sodium", precision = 10, scale = 2)
+    @Column(name = "sodium", precision = 10, scale = 3)
     private BigDecimal sodium;
 
-    @Column(name = "total_calories", precision = 10, scale = 2)
+    @Column(name = "total_calories", precision = 10, scale = 3)
     private BigDecimal totalCalories;
 
     @CreationTimestamp
--- src/main/java/com/jdc/recipe_service/domain/entity/RecipeIngredient.java
+++ src/main/java/com/jdc/recipe_service/domain/entity/RecipeIngredient.java
@@ -46,27 +46,27 @@ public class RecipeIngredient {
     @Builder.Default
     private Integer customPrice = 0;
 
-    @Column(name = "custom_calorie", precision=10, scale=2)
+    @Column(name = "custom_calorie", precision=10, scale=3)
     @Builder.Default
     private BigDecimal customCalorie = BigDecimal.ZERO;
 
-    @Column(name = "custom_carbohydrate", precision=10, scale=2)
+    @Column(name = "custom_carbohydrate", precision=10, scale=3)
     @Builder.Default
     private BigDecimal customCarbohydrate = BigDecimal.ZERO;
 
-    @Column(name = "custom_protein", precision=10, scale=2)
+    @Column(name = "custom_protein", precision=10, scale=3)
     @Builder.Default
     private BigDecimal customProtein = BigDecimal.ZERO;
 
-    @Column(name = "custom_fat", precision=10, scale=2)
+    @Column(name = "custom_fat", precision=10, scale=3)
     @Builder.Default
     private BigDecimal customFat = BigDecimal.ZERO;
 
-    @Column(name = "custom_sugar", precision=10, scale=2)
+    @Column(name = "custom_sugar", precision=10, scale=3)
     @Builder.Default
     private BigDecimal customSugar = BigDecimal.ZERO;
 
-    @Column(name = "custom_sodium", precision = 10, scale = 2)
+    @Column(name = "custom_sodium", precision = 10, scale = 3)
     @Builder.Default
     private BigDecimal customSodium = BigDecimal.ZERO;
 }
